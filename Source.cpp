b46e867ab5d1e876638138996375635e4b8e3670
ugandaknuckles <35495803+ugandaknuckles@users.noreply.github.com>
2018-01-25 08:11:26 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,28 @@
+#include<iostream>
+using namespace std;
+char n;
+char e;
+char s;
+
+int main() {
+	cout << "Hello!. If you want io enter the magical castle enter the password" << endl;
+	cout << "the password is 3 letters, enter 1 at a time" << endl;
+	cin >> n;
+	                     
+	cin >> e;
+
+	cin >> s;
+
+	if (n == 'N'&& e == 'E' && s == 'S')
+		cout <<"Welcome into the castle" << endl;
+	else cout << "get out of my swamp" << endl;
+
+	
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+
+	system("pause");
+}
